749ef66d338472f49094c759300b72ea30d3055a
Ilya Sokolov <developer.sokolov@gmail.com>
2022-12-21 15:05:22 +0300

    @feat: solution day1/part2

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -10,13 +10,22 @@ int main() {
     std::cout << p << std::endl;
     std::ifstream day1("../day1/data.txt");
     std::string s;
-    long int sum, max;
-    max = 0;
+    long int sum, elf1, elf2, elf3;
+    elf1 = 0;
+    elf2 = 0;
+    elf3 = 0;
     sum = 0;
     while (getline(day1, s)) {
         if (s.empty()) {
-            if (sum > max) {
-                max = sum;
+            if (sum > elf1) {
+                elf3 = elf2;
+                elf2 = elf1;
+                elf1 = sum;
+            } else if (elf2 < sum && elf1 != sum) {
+                elf3 = elf2;
+                elf2 = sum;
+            } else if (elf3 < sum && elf2 != sum && elf1 != sum) {
+                elf3 = sum;
             }
             sum = 0;
         } else {
@@ -24,7 +33,8 @@ int main() {
             sum += add;
         }
     }
+    std::cout << std::endl << elf1 << std::endl << elf2 << std::endl << elf3 << std::endl;
     std::cout << "RESULT:" << std::endl;
-    std::cout << max;
+    std::cout << elf1+elf2+elf3;
     return 0;
 }
