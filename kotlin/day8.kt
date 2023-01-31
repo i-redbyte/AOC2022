579a7d3fa4178765b574322b44434712616629d3
Ilya Sokolov <developer.sokolov@gmail.com>
2023-01-31 14:20:55 +0300

    @feat: day 8 solution part2

--- kotlin/day8.kt
+++ kotlin/day8.kt
@@ -1,45 +1,79 @@
 typealias Matrix<T> = List<List<T>>
 typealias MutableMatrix<T> = MutableList<MutableList<T>>
 
-fun <T, R> Matrix<T>.mapMatrixIndexed(modify: (Int, Int, T) -> R): Matrix<R> =
+val forest: Matrix<Int> = matrixOf(readInput("day8").map { row -> row.map { it.digitToInt() } })
+
+fun main() {
+    println("Solution:")
+    println("Part 1: ${solution1()}")
+    println("Part 2: ${solution2()}")
+
+}
+
+fun solution1() =
+    forest.mapMatrixIndexed { i, j, tree ->
+        forest.getPlaceToEnd(i, j).any { it.isEmpty() || it.all { t -> t < tree } }
+    }.count { it }
+
+fun solution2(): Int = forest.mapMatrixIndexed { i, j, tree -> calcViewScore(i, j, tree) }.matrixMax()
+
+private fun <T, R> Matrix<T>.mapMatrixIndexed(modify: (Int, Int, T) -> R): Matrix<R> =
     this.mapIndexed { i, row -> row.mapIndexed { j, col -> modify(i, j, col) } }
 
-fun <T> Matrix<T>.getRowToEnd(row: Int, col: Int): List<List<T>> =
+private fun <T> Matrix<T>.getRowToEnd(row: Int, col: Int): Matrix<T> =
     this[row].let { listOf(it.subList(0, col), it.subList(col + 1, it.size)) }
 
-fun <T> Matrix<T>.getColumn(col: Int): List<T> = getCol(this, col)
+private fun <T> Matrix<T>.getColumn(col: Int): List<T> = getColumnMatrix(this, col)
 
-fun <T> Matrix<T>.count(predicate: (T) -> Boolean) = this.sumOf { it.count(predicate) }
+private fun <T> Matrix<T>.count(predicate: (T) -> Boolean) = this.sumOf { it.count(predicate) }
 
-fun <T> Matrix<T>.getPlaceToEnd(row: Int, col: Int) = getColumnToEnd(row, col) + getRowToEnd(row, col)
+private fun <T> Matrix<T>.getPlaceToEnd(row: Int, col: Int) = getColumnToEnd(row, col) + getRowToEnd(row, col)
 
-fun <T> Matrix<T>.getColumnToEnd(row: Int, col: Int): List<List<T>> =
+private fun <T> Matrix<T>.getColumnToEnd(row: Int, col: Int): Matrix<T> =
     this.getColumn(col).let { listOf(it.subList(0, row), it.subList(row + 1, it.size)) }
 
-fun <T> getCol(array: List<List<T>>, col: Int): List<T> {
+private fun <T : Comparable<T>> Matrix<T>.matrixMax(): T = this.mapNotNull { it.maxOrNull() }.maxOrNull()!!
+
+private fun <T> getColumnMatrix(array: Matrix<T>, col: Int): List<T> {
     val rows = array.size
     val column = mutableListOf<T>()
     (0 until rows).forEach {
         try {
             column.add(array[it][col])
-        } catch (_: IndexOutOfBoundsException) {
+        } catch (_: IndexOutOfBoundsException) { /* no-op */
         }
     }
     return column
 }
 
-fun <T> matrixOf(rows: List<List<T>>): Matrix<T> = List(rows.size) { i -> rows[i] }
+private fun <T> matrixOf(rows: Matrix<T>): Matrix<T> = List(rows.size) { i -> rows[i] }
 
-fun main() {
-    val input = readInput("day8")
-    val forest = matrixOf(input.map { row -> row.map { it.digitToInt() } })
-    println("Solution:")
-    println("Part 1: ${solution1(forest)}")
+private operator fun <T> Matrix<T>.get(p: Place): T = this[p.y][p.x]
 
+private fun calcViewScore(row: Int, col: Int, tree: Int): Int =
+    Place.ways.map { look(Place(col, row), it, tree) }.reduce(Int::times)
+
+private fun look(place: Place, way: Place, tree: Int): Int {
+    val next =  way + place
+    return try {
+        if (forest[next] < tree)
+            1 + look(next, way, tree)
+        else 1
+    } catch (ex: IndexOutOfBoundsException) {
+        0
+    }
 }
 
-fun solution1(forest: Matrix<Int>) =
-    forest.mapMatrixIndexed { i, j, tree ->
-        forest.getPlaceToEnd(i, j).any { it.isEmpty() || it.all { t -> t < tree } }
-    }.count { it}
 
+data class Place(var x: Int, var y: Int) {
+    operator fun times(n: Int) = Place(x * n, y * n)
+    operator fun plus(other: Place) = Place(other.x + x, other.y + y)
+
+    companion object {
+        val LEFT = Place(-1, 0)
+        val RIGHT = Place(1, 0)
+        val UP = Place(0, -1)
+        val DOWN = Place(0, 1)
+        val ways = listOf(RIGHT, DOWN, LEFT, UP)
+    }
+}
